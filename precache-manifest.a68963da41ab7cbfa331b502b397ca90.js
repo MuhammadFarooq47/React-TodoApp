01984b2955bc0cfe05afd65d2b0fc3f68539c5f6
Muhammad Farooq <m.farooq.h7@gmail.com>
2021-01-24 22:14:48 -0800

    Updates

---
+++ precache-manifest.a68963da41ab7cbfa331b502b397ca90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558a6a63bfc08cac89b57ed22c5b480c",
+    "url": "/React-TodoApp/index.html"
+  },
+  {
+    "revision": "acca4645388a195dae2b",
+    "url": "/React-TodoApp/static/css/main.6d3a77c8.chunk.css"
+  },
+  {
+    "revision": "f7267f3d3231fe213893",
+    "url": "/React-TodoApp/static/js/2.0c73f94b.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/React-TodoApp/static/js/2.0c73f94b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acca4645388a195dae2b",
+    "url": "/React-TodoApp/static/js/main.6f45cf85.chunk.js"
+  },
+  {
+    "revision": "978343fb023b26565220",
+    "url": "/React-TodoApp/static/js/runtime-main.a7c414be.js"
+  }
+]);
